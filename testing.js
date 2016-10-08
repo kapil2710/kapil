124a34badc53ecb2b2c003d1010f71c1f2a94594
kapil2710 <kapilshrm28@gmail.com>
2016-10-08 10:58:00 +0530

    testing

---
+++ testing.js
@@ -0,0 +1,43 @@
+
+var a ="test"
+var b = 123
+var str = ['kapil']
+var num = [156755]
+var mix = ['ab',344,'gfd','233']
+var big = Number.NaN
+
+console.log(typeof str)
+console.log(typeof num)
+console.log(typeof mix
+	)
+console.log(typeof a
+	)
+console.log(typeof b
+	)
+
+
+var person =
+{
+	name : "kapil",
+
+	height : 6,
+
+	age : 20,
+	color : "white",
+	experience : 2,
+
+	address :
+			{
+				street1 : "madhu vihar near dharuhera chungi",
+				street2 : "rewari, (Haryana)",
+				pin : 123401,
+				city : "rewari"
+
+			}
+}
+
+
+console.log(person)
+
+
+
